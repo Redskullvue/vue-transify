9c90a3e40c7330f08cf1da5e4bd10a6039eb8d8f
“Redskullvue” <pouya.9996@gmail.com>
2025-10-01 17:05:04 +0330

    Add : Universal Props file

---
+++ src/components/commonProps.js
@@ -0,0 +1,6 @@
+export const commonProps = {
+  appear: { type: Boolean, default: false },
+  duration: { type: Number, default: 300 },
+  easing: { type: String, default: 'ease' },
+  delay: { type: Number, default: 0 },
+}
