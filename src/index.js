4600c919435a2629c1b7c076aecdc6a75ed16910
“Redskullvue” <pouya.9996@gmail.com>
2025-10-01 18:08:24 +0330

    Add : New Animations Added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 // This file is only used for exports to the built version
 import Fade from './components/Fade.vue'
+import SlideInLeft from './components/SlideInLeft.vue'
+import SlideInRight from './components/SlideInRight.vue'
 
-export default { Fade }
+export default {
+  Fade,
+  SlideInLeft,
+  SlideInRight,
+}
