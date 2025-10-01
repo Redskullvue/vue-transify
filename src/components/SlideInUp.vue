417cc6d752d674a17e03f8376996e590fe523f1e
“Redskullvue” <pouya.9996@gmail.com>
2025-10-01 18:11:51 +0330

    Feat : New SlideInUp Animation

---
+++ src/components/SlideInUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <Transition
+    name="slideInUp"
+    :appear="appear"
+    :style="{
+      '--animation-duration': duration + 'ms',
+      '--animation-easing': easing,
+      '--animation-delay': delay + 'ms',
+    }"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </Transition>
+</template>
+
+<script setup>
+import { commonProps } from './commonProps.js'
+const props = defineProps({ ...commonProps })
+</script>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // This file is only used for exports to the built version
-import Fade from './components/Fade.vue'
-import SlideInLeft from './components/SlideInLeft.vue'
-import SlideInRight from './components/SlideInRight.vue'
+import Fade from '@/components/Fade.vue'
+import SlideInLeft from '@/components/SlideInLeft.vue'
+import SlideInRight from '@/components/SlideInRight.vue'
+import SlideInUp from '@/components/SlideInUp.vue'
 
 export default {
   Fade,
   SlideInLeft,
   SlideInRight,
+  SlideInUp,
 }
